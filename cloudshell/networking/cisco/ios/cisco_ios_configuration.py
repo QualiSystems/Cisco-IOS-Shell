8193ef9b3c38d224e84332efbd4bb9d16eefcada
CoYe <wiseman.kostya@gmail.com>
2016-05-10 22:28:50 +0300

    added correct imports and configurations for 7.0

--- cloudshell/networking/cisco/ios/cisco_ios_configuration.py
+++ cloudshell/networking/cisco/ios/cisco_ios_configuration.py
@@ -1,13 +1,32 @@
 from cloudshell.networking.cisco.ios.cisco_ios_handler import CiscoIOSHandler
+from cloudshell.shell.core.context.context_utils import get_attribute_by_name
+import re, inject
 
 my_handler = CiscoIOSHandler
+
 
 def get_handler():
     return my_handler
 
-HANDLER_CLASS = CiscoIOSHandler
-DEFAULT_PROMPT = '.*>\s*$'
-ENABLE_PROMPT = '.*#\s*$'
-CONFIG_MODE_PROMPT = '\(config.*\)# *$'
 
-#DEFAULT_ACTIONS = CiscoIOSHandler._default_actions()
+HANDLER_CLASS = CiscoIOSHandler
+DEFAULT_PROMPT = '.*>\s*$|.*#\s*$'
+ENABLE_PROMPT = '.*#\s*$'
+CONFIG_MODE_PROMPT = '\(config.*\)#\s*$'
+
+@inject.params(logger='logger')
+def send_default_actions(session, logger=None):
+    """Send default commands to configure/clear session outputs
+    :return:
+    """
+    session.hardware_expect('terminal length 0', DEFAULT_PROMPT+'|'+ENABLE_PROMPT)
+    session.hardware_expect('terminal no exec prompt timestamp', DEFAULT_PROMPT+'|'+ENABLE_PROMPT)
+    session.hardware_expect(ENTER_CONFIG_MODE_PROMPT_COMMAND, CONFIG_MODE_PROMPT)
+    session.hardware_expect('no logging console', CONFIG_MODE_PROMPT)
+    session.hardware_expect('exit', DEFAULT_PROMPT+'|'+ENABLE_PROMPT)
+
+CONNECTION_TYPE = 'ssh'
+ENTER_CONFIG_MODE_PROMPT_COMMAND = 'configure terminal'
+EXIT_CONFIG_MODE_PROMPT_COMMAND = 'exit'
+DEFAULT_ACTIONS = send_default_actions
+
--- cloudshell/networking/cisco/ios/cisco_ios_resource_driver.py
+++ cloudshell/networking/cisco/ios/cisco_ios_resource_driver.py
@@ -1,13 +1,13 @@
 import inject
 
 from cloudshell.shell.core.context.context_utils import context_from_args
-from cloudshell.shell.core.driver_bootstrap import DriverBootstrap
+from cloudshell.networking.cisco.ios.cisco_ios_bootstrap import CiscoIOSBootstrap
 import cloudshell.networking.cisco.ios.cisco_ios_configuration as config
 
 
 class CiscoIOSResourceDriver:
     def __init__(self):
-        bootstrap = DriverBootstrap()
+        bootstrap = CiscoIOSBootstrap()
         bootstrap.add_config(config)
         bootstrap.initialize()
 
@@ -22,15 +22,91 @@ class CiscoIOSResourceDriver:
         pass
 
     @context_from_args
-    @inject.params(logger='logger', context='context', handler='handler')
-    def simple_command(self, context, command, logger=None, handler=None):
-        print(handler.send_command())
-        logger.info('Command completed')
-        return None
+    def apply_connectivity_changes(self, context, request):
+        handler = inject.instance('handler')
+        responce = handler.apply_connectivity_changes(request)
+        handler.logger.info('finished applying connectivity changes responce is:\n{0}'.format(str(responce)))
+        return responce
+
+    @context_from_args
+    def restore(self, context, source_file, clear_config='override'):
+        handler = inject.instance('handler')
+        responce = handler.restore_configuration(source_file=source_file, clear_config=clear_config)
+        handler.logger.info('Command completed')
+        return responce
+
+    @context_from_args
+    def save(self, context, destination_host, source_filename):
+        handler = inject.instance('handler')
+        responce = handler.backup_configuration(destination_host, source_filename)
+        return responce
 
     @context_from_args
     def get_inventory(self, context):
-        handler = inject.instance('handler')
-        test = handler.discover_snmp()
-        return test
+        """
+        Return device structure with all standard attributes
+        :return: result
+        :rtype: string
+        """
+        handler = inject.instance("handler")
+        result = handler.discover_snmp()
+        return result
 
+    @context_from_args
+    def load_firmware(self, context, remote_host, file_path):
+        """
+        Upload and updates firmware on the resource
+        :return: result
+        :rtype: string
+        """
+        handler = inject.instance("handler")
+        result_str = handler.update_firmware(remote_host=remote_host, file_path=file_path)
+        handler.disconnect()
+        handler.logger.info(result_str)
+
+    @context_from_args
+    def send_custom_command(self, context, command):
+        """
+        Send custom command
+        :return: result
+        :rtype: string
+        """
+        cli = inject.instance("cli_service")
+        result_str = cli.send_command(command)
+        return result_str
+
+    @context_from_args
+    def add_vlan(self, context, ports, vlan_range, port_mode, additional_info):
+        """
+        Assign vlan or vlan range to the certain interface
+        :return: result
+        :rtype: string
+        """
+        handler = inject.instance("handler")
+        result_str = handler.add_vlan(port_list=ports,
+                                      vlan_range=vlan_range.replace(' ', ''),
+                                      port_mode=port_mode,
+                                      qnq=('qnq' in additional_info.lower()))
+        handler.logger.info(result_str)
+
+    @context_from_args
+    def remove_vlan(self, context, ports, vlan_range, port_mode, additional_info):
+        """
+        Remove vlan or vlan range from the certain interface
+        :return: result
+        :rtype: string
+        """
+        handler = inject.instance("handler")
+        result_str = handler.remove_vlan(port_list=ports,
+                                         vlan_range=vlan_range, port_mode=port_mode)
+        handler.logger.info(result_str)
+
+    @context_from_args
+    def send_custom_config_command(self, context, command):
+        handler = inject.instance("handler")
+        result_str = handler.sendConfigCommand(cmd=command)
+        return handler.normalize_output(result_str)
+
+    @context_from_args
+    def shutdown(self, context):
+        pass

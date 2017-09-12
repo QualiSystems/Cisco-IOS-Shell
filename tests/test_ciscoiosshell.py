35df8daf6d9dd12081948eaec291ceaa5ded61a3
CoYe <wiseman.kostya@gmail.com>
2017-09-12 17:07:01 +0300

    fixed tests

--- tests/test_ciscoiosshell.py
+++ tests/test_ciscoiosshell.py
@@ -7,7 +7,7 @@ Tests for `CiscoiosshellDriver`
 import unittest
 from mock import patch
 
-from cloudshell.shell.core.driver_context import ResourceCommandContext
+from cloudshell.shell.core.context import ResourceCommandContext, ResourceContextDetails, ReservationContextDetails
 from src.cisco_ios_resource_driver import CiscoIOSResourceDriver
 
 
@@ -18,16 +18,19 @@ class TestCiscoIOSShellDriver(unittest.TestCase):
     def setUp(self):
         self.driver = CiscoIOSResourceDriver()
 
-    @patch('src.cisco_ios_resource_driver.get_attribute_by_name', return_value='1')
+    @patch('src.cisco_ios_resource_driver.create_networking_resource_from_context')
     @patch('src.cisco_ios_resource_driver.get_cli')
     def test_initialize(self, mocked_cli, mocked_get_attr, mocked_context, mocked_logger, mocked_api):
+        # Setup
+        mocked_get_attr.sessions_concurrency_limit.return_value = "1"
         # Act
         result = self.driver.initialize(mocked_context)
         # Assert
         self.assertTrue(result, 'Finished initializing')
 
+    @patch('src.cisco_ios_resource_driver.create_networking_resource_from_context')
     @patch('src.cisco_ios_resource_driver.AutoloadRunner')
-    def test_get_inventory(self, mocked_class, mocked_context, mocked_logger, mocked_api):
+    def test_get_inventory(self, mocked_class, mocked_get_attr, mocked_context, mocked_logger, mocked_api):
         # Arrange
         mocked_class.return_value.discover.return_value = ''
 
@@ -37,8 +40,9 @@ class TestCiscoIOSShellDriver(unittest.TestCase):
         # Assert
         mocked_class.return_value.discover.assert_called()
 
+    @patch('src.cisco_ios_resource_driver.create_networking_resource_from_context')
     @patch('src.cisco_ios_resource_driver.CommandRunner')
-    def test_run_custom_command(self, mocked_class, mocked_context, mocked_logger, mocked_api):
+    def test_run_custom_command(self, mocked_class, mocked_get_attr, mocked_context, mocked_logger, mocked_api):
         # Arrange
         command = 'test command'
         response = 'response'
@@ -51,8 +55,9 @@ class TestCiscoIOSShellDriver(unittest.TestCase):
         self.assertTrue(response, result)
         mocked_class.return_value.run_custom_command.assert_called_with(custom_command=command)
 
+    @patch('src.cisco_ios_resource_driver.create_networking_resource_from_context')
     @patch('src.cisco_ios_resource_driver.StateRunner')
-    def test_health_check(self, mocked_class, mocked_context, mocked_logger, mocked_api):
+    def test_health_check(self, mocked_class, mocked_get_attr, mocked_context, mocked_logger, mocked_api):
         # Arrange
         response = 'response'
         mocked_class.return_value.health_check.return_value = response
@@ -64,8 +69,9 @@ class TestCiscoIOSShellDriver(unittest.TestCase):
         self.assertTrue(response, result)
         mocked_class.return_value.health_check.assert_called_with()
 
+    @patch('src.cisco_ios_resource_driver.create_networking_resource_from_context')
     @patch('src.cisco_ios_resource_driver.CommandRunner')
-    def test_run_custom_config_command(self, mocked_class, mocked_context, mocked_logger, mocked_api):
+    def test_run_custom_config_command(self, mocked_class, mocked_get_attr, mocked_context, mocked_logger, mocked_api):
         # Arrange
         command = 'test command'
         response = 'response'
@@ -78,8 +84,9 @@ class TestCiscoIOSShellDriver(unittest.TestCase):
         self.assertTrue(response, result)
         mocked_class.return_value.run_custom_config_command.assert_called_with(custom_command=command)
 
+    @patch('src.cisco_ios_resource_driver.create_networking_resource_from_context')
     @patch('src.cisco_ios_resource_driver.CommandRunner')
-    def test_send_custom_command(self, mocked_class, mocked_context, mocked_logger, mocked_api):
+    def test_send_custom_command(self, mocked_class, mocked_get_attr, mocked_context, mocked_logger, mocked_api):
         # Arrange
         command = 'test command'
         response = 'response'
@@ -92,8 +99,9 @@ class TestCiscoIOSShellDriver(unittest.TestCase):
         self.assertTrue(response, result)
         mocked_class.return_value.run_custom_command.assert_called_with(custom_command=command)
 
+    @patch('src.cisco_ios_resource_driver.create_networking_resource_from_context')
     @patch('src.cisco_ios_resource_driver.CommandRunner')
-    def test_send_custom_config_command(self, mocked_class, mocked_context, mocked_logger, mocked_api):
+    def test_send_custom_config_command(self, mocked_class, mocked_get_attr, mocked_context, mocked_logger, mocked_api):
         # Arrange
         command = 'test command'
         response = 'response'
@@ -106,8 +114,9 @@ class TestCiscoIOSShellDriver(unittest.TestCase):
         self.assertTrue(response, result)
         mocked_class.return_value.run_custom_config_command.assert_called_with(custom_command=command)
 
+    @patch('src.cisco_ios_resource_driver.create_networking_resource_from_context')
     @patch('src.cisco_ios_resource_driver.FirmwareRunner')
-    def test_load_firmware(self, mocked_class, mocked_context, mocked_logger, mocked_api):
+    def test_load_firmware(self, mocked_class, mocked_get_attr, mocked_context, mocked_logger, mocked_api):
         # Arrange
         path = 'test'
         vrf_management_name = 'response'
@@ -120,13 +129,13 @@ class TestCiscoIOSShellDriver(unittest.TestCase):
         mocked_class.return_value.load_firmware.assert_called_with(path=path, vrf_management_name=vrf_management_name)
 
     @patch('src.cisco_ios_resource_driver.FirmwareRunner')
-    @patch('src.cisco_ios_resource_driver.get_attribute_by_name')
+    @patch('src.cisco_ios_resource_driver.create_networking_resource_from_context')
     def test_load_firmware_no_vrf(self, mocked_get_attr, mocked_class, mocked_context, mocked_logger, mocked_api):
         # Arrange
         path = 'test'
         vrf_management_name = None
         mocked_class.return_value.load_firmware.return_value = ''
-        mocked_get_attr.return_value = vrf_management_name
+        mocked_get_attr.return_value.vrf_management_name = vrf_management_name
 
         # Act
         self.driver.load_firmware(mocked_context, path=path, vrf_management_name=vrf_management_name)
@@ -135,14 +144,14 @@ class TestCiscoIOSShellDriver(unittest.TestCase):
         mocked_class.return_value.load_firmware.assert_called_with(path=path, vrf_management_name=vrf_management_name)
 
     @patch('src.cisco_ios_resource_driver.FirmwareRunner')
-    @patch('src.cisco_ios_resource_driver.get_attribute_by_name')
+    @patch('src.cisco_ios_resource_driver.create_networking_resource_from_context')
     def test_update_firmware(self, mocked_get_attr, mocked_class, mocked_context, mocked_logger, mocked_api):
         # Arrange
         remote_host = 'test'
         file_path = 'response'
         vrf_management_name = None
         mocked_class.return_value.load_firmware.return_value = ''
-        mocked_get_attr.return_value = vrf_management_name
+        mocked_get_attr.return_value.vrf_management_name = vrf_management_name
 
         # Act
         self.driver.update_firmware(mocked_context, remote_host=remote_host, file_path=file_path)
@@ -152,12 +161,12 @@ class TestCiscoIOSShellDriver(unittest.TestCase):
                                                                    vrf_management_name=vrf_management_name)
 
     @patch('src.cisco_ios_resource_driver.ConfigurationRunner')
-    @patch('src.cisco_ios_resource_driver.get_attribute_by_name')
+    @patch('src.cisco_ios_resource_driver.create_networking_resource_from_context')
     def test_save_no_params(self, mocked_get_attr, mocked_class, mocked_context, mocked_logger, mocked_api):
         # Arrange
         mocked_class.return_value.save.return_value = ''
         vrf_management_name = 'default'
-        mocked_get_attr.return_value = vrf_management_name
+        mocked_get_attr.return_value.vrf_management_name = vrf_management_name
 
         # Act
         self.driver.save(mocked_context, folder_path='', configuration_type='', vrf_management_name='')
@@ -166,8 +175,9 @@ class TestCiscoIOSShellDriver(unittest.TestCase):
         mocked_class.return_value.save.assert_called_with(folder_path='', configuration_type='running',
                                                           vrf_management_name=vrf_management_name)
 
+    @patch('src.cisco_ios_resource_driver.create_networking_resource_from_context')
     @patch('src.cisco_ios_resource_driver.ConfigurationRunner')
-    def test_save_all_params(self, mocked_class, mocked_context, mocked_logger, mocked_api):
+    def test_save_all_params(self, mocked_class, mocked_get_attr, mocked_context, mocked_logger, mocked_api):
         # Arrange
         folder_path = 'ftp://ftpuser:ftppass@server/folder'
         configuration_type = 'running'
@@ -185,14 +195,14 @@ class TestCiscoIOSShellDriver(unittest.TestCase):
                                                           vrf_management_name=vrf_management_name)
 
     @patch('src.cisco_ios_resource_driver.ConfigurationRunner')
-    @patch('src.cisco_ios_resource_driver.get_attribute_by_name')
+    @patch('src.cisco_ios_resource_driver.create_networking_resource_from_context')
     def test_save_no_vrf(self, mocked_get_attr, mocked_class, mocked_context, mocked_logger, mocked_api):
         # Arrange
         folder_path = 'ftp://ftpuser:ftppass@server/folder'
         configuration_type = 'running'
         vrf = 'default'
         mocked_class.return_value.save.return_value = ''
-        mocked_get_attr.return_value = vrf
+        mocked_get_attr.return_value.vrf_management_name = vrf
 
         # Act
         self.driver.save(mocked_context, folder_path=folder_path, configuration_type=configuration_type,
@@ -204,14 +214,14 @@ class TestCiscoIOSShellDriver(unittest.TestCase):
                                                           vrf_management_name=vrf)
 
     @patch('src.cisco_ios_resource_driver.ConfigurationRunner')
-    @patch('src.cisco_ios_resource_driver.get_attribute_by_name')
+    @patch('src.cisco_ios_resource_driver.create_networking_resource_from_context')
     def test_save_no_vrf_no_folder_path(self, mocked_get_attr, mocked_class, mocked_context, mocked_logger, mocked_api):
         # Arrange
         configuration_type = 'startup'
 
         vrf = 'default'
         mocked_class.return_value.save.return_value = ''
-        mocked_get_attr.return_value = vrf
+        mocked_get_attr.return_value.vrf_management_name = vrf
 
         # Act
         self.driver.save(mocked_context, configuration_type=configuration_type,
@@ -223,13 +233,13 @@ class TestCiscoIOSShellDriver(unittest.TestCase):
                                                           vrf_management_name=vrf)
 
     @patch('src.cisco_ios_resource_driver.ConfigurationRunner')
-    @patch('src.cisco_ios_resource_driver.get_attribute_by_name')
+    @patch('src.cisco_ios_resource_driver.create_networking_resource_from_context')
     def test_save_no_vrf_no_config_file(self, mocked_get_attr, mocked_class, mocked_context, mocked_logger, mocked_api):
         # Arrange
         folder_path = 'ftp://ftpuser:ftppass@server/folder'
         vrf = 'default'
         mocked_class.return_value.save.return_value = ''
-        mocked_get_attr.return_value = vrf
+        mocked_get_attr.return_value.vrf_management_name = vrf
 
         # Act
         self.driver.save(mocked_context, folder_path=folder_path, configuration_type='', vrf_management_name='')
@@ -239,8 +249,9 @@ class TestCiscoIOSShellDriver(unittest.TestCase):
                                                           folder_path=folder_path,
                                                           vrf_management_name=vrf)
 
+    @patch('src.cisco_ios_resource_driver.create_networking_resource_from_context')
     @patch('src.cisco_ios_resource_driver.ConfigurationRunner')
-    def test_restore_all_params(self, mocked_class, mocked_context, mocked_logger, mocked_api):
+    def test_restore_all_params(self, mocked_class, mocked_get_attr, mocked_context, mocked_logger, mocked_api):
         # Arrange
         path = 'ftp://ftpuser:ftppass@server/folder'
         configuration_type = 'startup'
@@ -258,7 +269,7 @@ class TestCiscoIOSShellDriver(unittest.TestCase):
                                                              vrf_management_name=vrf_management_name)
 
     @patch('src.cisco_ios_resource_driver.ConfigurationRunner')
-    @patch('src.cisco_ios_resource_driver.get_attribute_by_name')
+    @patch('src.cisco_ios_resource_driver.create_networking_resource_from_context')
     def test_restore_no_vrf(self, mocked_get_attr, mocked_class, mocked_context, mocked_logger, mocked_api):
         # Arrange
         path = 'ftp://ftpuser:ftppass@server/folder'
@@ -266,7 +277,7 @@ class TestCiscoIOSShellDriver(unittest.TestCase):
         restore_method = 'append'
         vrf = 'management'
         mocked_class.return_value.restore.return_value = ''
-        mocked_get_attr.return_value = vrf
+        mocked_get_attr.return_value.vrf_management_name = vrf
 
         # Act
         self.driver.restore(mocked_context, path=path, configuration_type=configuration_type,
@@ -277,14 +288,14 @@ class TestCiscoIOSShellDriver(unittest.TestCase):
                                                              restore_method=restore_method, vrf_management_name=vrf)
 
     @patch('src.cisco_ios_resource_driver.ConfigurationRunner')
-    @patch('src.cisco_ios_resource_driver.get_attribute_by_name')
+    @patch('src.cisco_ios_resource_driver.create_networking_resource_from_context')
     def test_restore_no_vrf_no_restore_method(self, mocked_get_attr, mocked_class, mocked_context, mocked_logger, mocked_api):
         # Arrange
         path = 'ftp://ftpuser:ftppass@server/folder'
         configuration_type = 'startup'
         vrf = 'management'
         mocked_class.return_value.restore.return_value = ''
-        mocked_get_attr.return_value = vrf
+        mocked_get_attr.return_value.vrf_management_name = vrf
 
         # Act
         self.driver.restore(mocked_context, path=path, configuration_type=configuration_type, restore_method='',
@@ -295,12 +306,12 @@ class TestCiscoIOSShellDriver(unittest.TestCase):
                                                              restore_method='override', vrf_management_name=vrf)
 
     @patch('src.cisco_ios_resource_driver.ConfigurationRunner')
-    @patch('src.cisco_ios_resource_driver.get_attribute_by_name')
+    @patch('src.cisco_ios_resource_driver.create_networking_resource_from_context')
     def test_restore_no_optional_params(self, mocked_get_attr, mocked_class, mocked_context, mocked_logger, mocked_api):
         # Arrange
         path = 'ftp://ftpuser:ftppass@server/folder'
         mocked_class.return_value.restore.return_value = ''
-        mocked_get_attr.return_value = None
+        mocked_get_attr.return_value.vrf_management_name = None
 
         # Act
         self.driver.restore(mocked_context, path=path,
@@ -312,8 +323,9 @@ class TestCiscoIOSShellDriver(unittest.TestCase):
         mocked_class.return_value.restore.assert_called_with(path=path, configuration_type='running',
                                                              restore_method='override', vrf_management_name=None)
 
+    @patch('src.cisco_ios_resource_driver.create_networking_resource_from_context')
     @patch('src.cisco_ios_resource_driver.ConfigurationRunner')
-    def test_orchestration_save_no_optional_params(self, mocked_class, mocked_context, mocked_logger, mocked_api):
+    def test_orchestration_save_no_optional_params(self, mocked_class, mocked_get_attr, mocked_context, mocked_logger, mocked_api):
         # Arrange
         mocked_class.return_value.orchestration_save.return_value = ''
 
@@ -323,8 +335,9 @@ class TestCiscoIOSShellDriver(unittest.TestCase):
         # Assert
         mocked_class.return_value.orchestration_save.assert_called_with(mode='shallow', custom_params=None)
 
+    @patch('src.cisco_ios_resource_driver.create_networking_resource_from_context')
     @patch('src.cisco_ios_resource_driver.ConfigurationRunner')
-    def test_orchestration_save(self, mocked_class, mocked_context, mocked_logger, mocked_api):
+    def test_orchestration_save(self, mocked_class, mocked_get_attr, mocked_context, mocked_logger, mocked_api):
         # Arrange
         mode = 'shallow'
         custom_params = 'test json'
@@ -336,8 +349,9 @@ class TestCiscoIOSShellDriver(unittest.TestCase):
         # Assert
         mocked_class.return_value.orchestration_save.assert_called_with(mode=mode, custom_params=custom_params)
 
+    @patch('src.cisco_ios_resource_driver.create_networking_resource_from_context')
     @patch('src.cisco_ios_resource_driver.ConfigurationRunner')
-    def test_orchestration_save_no_custom_params(self, mocked_class, mocked_context, mocked_logger, mocked_api):
+    def test_orchestration_save_no_custom_params(self, mocked_class, mocked_get_attr, mocked_context, mocked_logger, mocked_api):
         # Arrange
         mocked_class.return_value.orchestration_save.return_value = ''
 
@@ -347,8 +361,9 @@ class TestCiscoIOSShellDriver(unittest.TestCase):
         # Assert
         mocked_class.return_value.orchestration_save.assert_called_with(mode='shallow', custom_params=None)
 
+    @patch('src.cisco_ios_resource_driver.create_networking_resource_from_context')
     @patch('src.cisco_ios_resource_driver.ConfigurationRunner')
-    def test_orchestration_restore_no_custom_params(self, mocked_class, mocked_context, mocked_logger, mocked_api):
+    def test_orchestration_restore_no_custom_params(self, mocked_class, mocked_get_attr, mocked_context, mocked_logger, mocked_api):
         # Arrange
         saved_artifact_info = 'test json'
         custom_params = None
@@ -361,8 +376,9 @@ class TestCiscoIOSShellDriver(unittest.TestCase):
         mocked_class.return_value.orchestration_restore.assert_called_with(saved_artifact_info=saved_artifact_info,
                                                                            custom_params=None)
 
+    @patch('src.cisco_ios_resource_driver.create_networking_resource_from_context')
     @patch('src.cisco_ios_resource_driver.ConfigurationRunner')
-    def test_orchestration_restore_all_params(self, mocked_class, mocked_context, mocked_logger, mocked_api):
+    def test_orchestration_restore_all_params(self, mocked_class, mocked_get_attr, mocked_context, mocked_logger, mocked_api):
         # Arrange
         saved_artifact_info = 'test json'
         custom_params = 'test json'
@@ -375,8 +391,9 @@ class TestCiscoIOSShellDriver(unittest.TestCase):
         mocked_class.return_value.orchestration_restore.assert_called_with(saved_artifact_info=saved_artifact_info,
                                                                            custom_params=custom_params)
 
+    @patch('src.cisco_ios_resource_driver.create_networking_resource_from_context')
     @patch('src.cisco_ios_resource_driver.ConnectivityRunner')
-    def test_apply_connectivity_changes(self, mocked_class, mocked_context, mocked_logger, mocked_api):
+    def test_apply_connectivity_changes(self, mocked_class, mocked_get_attr, mocked_context, mocked_logger, mocked_api):
         # Arrange
         request = 'test json'
         mocked_class.return_value.apply_connectivity_changes.return_value = ''
